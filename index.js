ffaaf6625ed68c4fa513da17afebb86c376397ae
Anthony Kiplimo <130223066+anthonylimo90@users.noreply.github.com>
2023-06-01 11:33:57 +0000

    Small edits

--- index.js
+++ index.js
@@ -6,89 +6,89 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-const identity = new okra_client.Identity(process.env.OKRA_SECRET, process.env.OKRA_ENV)
+const identity = new okra_client.Identity(process.env.OKRA_SECRET, process.env.OKRA_ENV);
 
 app.get("/", (req, res) => {
     res.send({message:"Hello"})
-})
+});
 
 // BVN Verification
 
 app.post("/bvn-verify", (req, res) => {
-    const customerBvn = req.body.customerBvn
+    const customerBvn = req.body.customerBvn;
 
     const options = {
         bvn: customerBvn
-    }
+    };
 
     identity.verify(options)
         .then(({data}) => {
-            console.log(`Data: ${data}`)
-            res.send(data)
+            console.log(`Data: ${data}`);
+            res.send(data);
         })
-        .catch((err)=> console.error("Error: " + err))
-})
+        .catch((err)=> console.error("Error: " + err));
+});
 
 // Bulk BVN Verification
 
 app.post("/bulk-bvn-verify", (req, res) => {
-    const bnvList = req.body.bnvList
+    const bnvList = req.body.bnvList;
 
     const options = {
         bvn: bnvList
-    }
+    };
 
     identity.verify(options)
         .then(({data}) => {
-            console.log(`Data: ${data}`)
-            res.send(data)
+            console.log(`Data: ${data}`);
+            res.send(data);
         })
-        .catch((err) => console.error(`Something went wrong: ${err}`))
-})
+        .catch((err) => console.error(`Something went wrong: ${err}`));
+});
 
 // Nuban Verification
 
 app.post("/nuban-verification", (req, res) => {
-    const nubanNumber = req.body.nubanNumber
-    const nubanBank = req.body.nubanBank
+    const nubanNumber = req.body.nubanNumber;
+    const nubanBank = req.body.nubanBank;
 
     const options = {
         nubanNumber: nubanNumber,
         nubanBank: nubanBank
-    }
+    };
 
     identity.verify(options)
         .then(({data}) => {
-            console.log(`Data: ${data}`)
-            res.send(data)
+            console.log(`Data: ${data}`);
+            res.send(data);
         })
-        .catch(err => console.error(`Something went wrong: ${err}`))
-})
+        .catch(err => console.error(`Something went wrong: ${err}`));
+});
 
 // Get identity by customer ID
 
 app.post("/get-by-customer-id", (req, res) => {
     
-    const customerId = req.body.customerId
+    const customerId = req.body.customerId;
 
     const options = {
         id: customerId
-    }
+    };
     
     identity.get(options)
         .then(({data}) => {
-            console.log(`Data: ${data}`)
-            res.send(data)
+            console.log(`Data: ${data}`);
+            res.send(data);
         })
-        .catch(err => console.error(`Something went wrong: ${err}`))
-})
+        .catch(err => console.error(`Something went wrong: ${err}`));
+});
 
 // Start server
 
 app.listen(3000, (err) => {
     if(!err) console.log("App running on port 3000")
-    else console.error(`Something went wrong ${err}`)
-})
+    else console.error(`Something went wrong ${err}`);
+});
